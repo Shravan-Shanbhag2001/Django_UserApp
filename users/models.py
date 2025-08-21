065d39ed7c446909f6b65572aa1c1ff62b843c7a
Shravan-Shanbhag2001 <78205410+Shravan-Shanbhag2001@users.noreply.github.com>
2025-08-21 21:57:54 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,12 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username must be set")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-    name = models.CharField(max_length=255, blank=False, null=False)
-    mobile_number = models.CharField(max_length=15, blank=False, null=False)
-    city = models.CharField(max_length=255, blank=False, null=False)
+    name = models.CharField(max_length=255)
+    mobile_number = models.CharField(max_length=15)
+    city = models.CharField(max_length=255)
     referral_code = models.CharField(max_length=100, blank=True, null=True)
     referrer_code = models.CharField(max_length=100, unique=True)
-    
-    REQUIRED_FIELDS = ['name', 'mobile_number','city'] 
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'username' 
+    REQUIRED_FIELDS = ['name', 'mobile_number', 'city']
