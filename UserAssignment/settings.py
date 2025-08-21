625aec1134f1ec466fbc3e1857083b31a1080925
Shravan-Shanbhag2001 <78205410+Shravan-Shanbhag2001@users.noreply.github.com>
2025-08-21 20:50:06 +0530

    Update settings.py

--- UserAssignment/settings.py
+++ UserAssignment/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'users',
     'rest_framework',
 ]
+CORS_ALLOWED_ORIGINS = ["*"]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
